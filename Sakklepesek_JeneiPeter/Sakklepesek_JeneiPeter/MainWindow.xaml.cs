42a49c657efd7c12632e57075f1fd70aba4db687
Jenei Péter <jeneipeter001@gmail.com>
2022-05-23 15:57:14 +0200

    Tábla generálás kódban

--- Sakklepesek_JeneiPeter/Sakklepesek_JeneiPeter/MainWindow.xaml.cs
+++ Sakklepesek_JeneiPeter/Sakklepesek_JeneiPeter/MainWindow.xaml.cs
@@ -28,7 +28,38 @@ namespace Sakklepesek_JeneiPeter
 
         void TablaLetrehozas()
         {
-            
+            for (int i = 0; i < 8; i++)
+            {
+                tablaGrid.RowDefinitions.Add(new RowDefinition());
+                tablaGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            BrushConverter bc = new BrushConverter();
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Rectangle mezo = new Rectangle();
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                            mezo.Fill = (Brush)(bc.ConvertFrom("#ebecd0"));
+                        else
+                            mezo.Fill = (Brush)(bc.ConvertFrom("#779556"));
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                            mezo.Fill = (Brush)(bc.ConvertFrom("#779556"));
+                        else
+                            mezo.Fill = (Brush)(bc.ConvertFrom("#ebecd0"));
+                    }
+
+                    tablaGrid.Children.Add(mezo);
+                    Grid.SetRow(mezo, i);
+                    Grid.SetColumn(mezo, j);
+                }
+            }
         }
     }
 }
