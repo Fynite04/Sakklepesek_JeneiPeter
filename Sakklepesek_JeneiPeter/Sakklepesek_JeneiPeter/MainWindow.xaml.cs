097e9fa15cf3de73e5d93a9c9ac711d964c1aa2e
Jenei Péter <jeneipeter001@gmail.com>
2022-05-25 19:16:01 +0200

    Figura elhelyezés és korlátozás a legördülő menüben

--- Sakklepesek_JeneiPeter/Sakklepesek_JeneiPeter/MainWindow.xaml.cs
+++ Sakklepesek_JeneiPeter/Sakklepesek_JeneiPeter/MainWindow.xaml.cs
@@ -21,14 +21,22 @@ namespace Sakklepesek_JeneiPeter
     public partial class MainWindow : Window
     {
         Rectangle[,] sakkTabla = new Rectangle[8,8];
+        List<int> figurakSzama = new List<int>();
 
         public MainWindow()
         {
             InitializeComponent();
             TablaLetrehozas();
+            figurakSzama.Add(1); // 0: kiráy
+            figurakSzama.Add(1); // 1: kiráynő
+            figurakSzama.Add(2); // 2: bástya           
+            figurakSzama.Add(2); // 3: futó
+            figurakSzama.Add(2); // 4: huszár
+            figurakSzama.Add(8); // 5: fehér gyalog
+            figurakSzama.Add(8); // 6: fekete gyalog
         }
 
-        void TablaLetrehozas()
+        private void TablaLetrehozas()
         {
             for (int i = 0; i < 8; i++)
             {
@@ -74,17 +82,67 @@ namespace Sakklepesek_JeneiPeter
         private void MezoKlikk(object sender, MouseButtonEventArgs e)
         {
             Rectangle mezo = (Rectangle)sender;
+            int posX = 0;
+            int posY = 0;
 
-            // Pozíció
+            // Pozíció label
             string[] pozBetuk = { "A", "B", "C", "D", "E", "F", "G", "H"};
             for (int y = 0; y < 8; y++)
             {
                 for (int x = 0; x < 8; x++)
                 {
                     if (sakkTabla[y, x].Equals(mezo))
-                        pozicio_Lbl.Content = $"Pozíció: {pozBetuk[x]}{8 - y}";
+                    {
+                        posX = x;
+                        posY = y;
+                    }
                 }
             }
+            pozicio_Lbl.Content = $"Pozíció: {pozBetuk[posX]}{8 - posY}";
+
+            // LÉPÉS
+            int kijelolt = figura_CBx.SelectedIndex;
+
+            // Király
+            if (kijelolt == 0)
+            {
+                FiguraElhelyezo(posX, posY, "kiraly");
+            }
+
+            FiguraSzamKorlatozo(kijelolt);
+            figura_CBx.SelectedIndex = -1;
+        }
+
+        private void FiguraElhelyezo(int x, int y, string kepFajl)
+        {
+            Rectangle figura = new Rectangle();
+            figura.Height = 50;
+            figura.Width = 50;
+            var posLeft = 35 + x * 50;
+            var posTop = 40 + y * 50;
+
+            foCanvas.Children.Add(figura);
+            Canvas.SetLeft(figura, posLeft);
+            Canvas.SetTop(figura, posTop);
+
+            ImageBrush imgBrush = new ImageBrush();
+            imgBrush.ImageSource = new BitmapImage(new Uri($"Img/{kepFajl}.png", UriKind.Relative));
+            figura.Fill = imgBrush;
+        }
+
+        private void FiguraSzamKorlatozo(int index)
+        {
+            figurakSzama[index]--;
+
+            if (figurakSzama[index] == 0)
+            {
+                ComboBoxItem kijelolt = (ComboBoxItem)figura_CBx.SelectedItem;
+                kijelolt.IsEnabled = false;
+            }
+        }
+        private void MezoSzinezo(int x, int y, string hexSzin)
+        {
+
         }
     }
 }
